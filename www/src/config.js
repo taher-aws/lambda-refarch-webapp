5aed7b8d52bf6c65c7f4140636e5e0d56b13de11
Taher Mulla <tamulla@amazon.com>
2025-11-07 17:12:09 -0600

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "5thtf3ao8bbjrkmh5mop1b2mrk",     // CognitoClientID
+  "api_base_url": "https://kgbutkcdge.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-todo-fullstack-webapp-with-auth-6t7y.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.dnvrxkrwzswox.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
