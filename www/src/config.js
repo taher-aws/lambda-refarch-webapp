897712f5c0f7d2bfce7c176f0b4f37566fbae240
Taher Mulla <tamulla@amazon.com>
2025-11-07 17:29:24 -0600

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "5thtf3ao8bbjrkmh5mop1b2mrk",     // CognitoClientID
-  "api_base_url": "https://kgbutkcdge.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://kgbutkcdge.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-todo-fullstack-webapp-with-auth-6t7y.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.dnvrxkrwzswox.amplifyapp.com"                                      // AmplifyURL
 };
